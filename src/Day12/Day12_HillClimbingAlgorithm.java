ccf282ae97084879fa6afcfcba0ccbf20567e017
miveko <m.kostov.igcse.ict@gmail.com>
2022-12-12 23:15:19 +0200

    Starting project Day12_HillClimbingAlgorithm

---
+++ src/Day12/Day12_HillClimbingAlgorithm.java
@@ -0,0 +1,15 @@
+package Day12;
+
+import Base.Puzzle;
+import java.util.Scanner;
+
+public class Day12_HillClimbingAlgorithm extends Puzzle{
+    public static void main(String[] args) {
+        Puzzle puzzle = new Day12_HillClimbingAlgorithm();
+        puzzle.execute(args);
+    }
+
+    protected void solution(Scanner inputReader) {
+
+    }
+}
