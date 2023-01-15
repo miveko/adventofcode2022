3690358fffd6c6704fd4f4727ef070e64164544b
miveko <m.kostov.igcse.ict@gmail.com>
2023-01-16 00:45:36 +0200

    Adding findExactBestPath for Day24_BlizzardBasin (not a required extension)

--- src/Day24/Day24_BlizzardBasin.java
+++ src/Day24/Day24_BlizzardBasin.java
@@ -7,15 +7,17 @@ import java.util.List;
 
 public class Day24_BlizzardBasin extends Puzzle {
 
-    List<Blizzard> blizzard;
-    Blizzard me;
-    List<String> unUsedOptions;
+    List<Blizzard> blizzard, blizzardInitial;
     Set<Point> stormed;
     Set<Point>  reached;
     int minute, width, height;
+    List<Point> steps;          //Used only by the findExactBestPath part
+    //Used only in the Brute force solution:
+    Blizzard me;
+    String path;
     Map<Integer, List<Blizzard>> blizzardsState;
     Map<Integer, Point> myState;
-    String path;
+    List<String> unUsedOptions;
 
     public static void main(String[] args) {
         Puzzle puzzle = new Day24_BlizzardBasin();
@@ -24,25 +26,36 @@ public class Day24_BlizzardBasin extends Puzzle {
 
     @Override
     protected void solution(Scanner inputReader) {
-        blizzard = new ArrayList<>();
+        blizzardInitial = new ArrayList<>();
         width = inputReader.nextLine().length();
         height = 1;
         while (inputReader.hasNextLine()) {
             String line = inputReader.nextLine();
             for(int j = 0; j < line.length(); j++)
                 if(line.charAt(j) != '#' && line.charAt(j) != '.')
-                    blizzard.add(new Blizzard(new Point(height, j), line.charAt(j)));
+                    blizzardInitial.add(new Blizzard(new Point(height, j), line.charAt(j)));
             height++;
         }
 
+        resetBlizzardArray();
         Point start = new Point(0, 1);
         Point finish = new Point(height - 1, width - 2);
-        int minutes = findShortestTime(start, finish);
-        setAnswerPartOne(String.valueOf(minutes));
+        int minutes_partOne = findShortestTime(start, finish);
+        setAnswerPartOne(String.valueOf(minutes_partOne));
 
-        minutes += findShortestTime(finish, start);
-        minutes += findShortestTime(start, finish);
-        setAnswerPartTwo(String.valueOf(minutes));
+        int minutes_partTwo = minutes_partOne + findShortestTime(finish, start);
+        minutes_partTwo += findShortestTime(start, finish);
+        setAnswerPartTwo(String.valueOf(minutes_partTwo));
+
+        steps = new ArrayList<>();
+        steps.add(new Point(finish));
+        findExactBestPath(minutes_partOne, start, finish);
+    }
+
+    private void resetBlizzardArray() {
+        blizzard = new ArrayList<>();
+        for(Blizzard b : blizzardInitial)
+            blizzard.add(new Blizzard(b));
     }
 
     private int findShortestTime(Point start, Point finish) {
@@ -70,7 +83,7 @@ public class Day24_BlizzardBasin extends Puzzle {
 
     private void updateReachedList(Point pos, Point destination) {
         Point nextPoint = new Point(pos.x + 1, pos.y);
-        if(nextPoint.x == destination.x && nextPoint.y == destination.y) {
+        if(nextPoint.x == destination.x && nextPoint.y == destination.y && destination.x == height - 1) {
             reached.add(new Point(destination.x, destination.y)); //getting to the Final destination
             return;
         }
@@ -87,7 +100,7 @@ public class Day24_BlizzardBasin extends Puzzle {
             reached.add(new Point(nextPoint));
 
         nextPoint.x--;
-        if(nextPoint.x == destination.x && nextPoint.y == destination.y) {
+        if(nextPoint.x == destination.x && nextPoint.y == destination.y && destination.x == 0) {
             reached.add(new Point(destination.x, destination.y)); //getting to the Final destination
             return;
         }
@@ -100,29 +113,50 @@ public class Day24_BlizzardBasin extends Puzzle {
             reached.add(new Point(nextPoint));
     }
 
-    private char updatePossibleMove(char nextMove, char possibleMove) {
-        if(nextMove == ' ')
-            nextMove = possibleMove;
-        else {
-            String lastOptions = "";
-            if(unUsedOptions.size() > 0)
-                lastOptions = unUsedOptions.get(unUsedOptions.size() - 1);
-            if(lastOptions.startsWith(String.valueOf(minute))) {
-                lastOptions += possibleMove;
-                unUsedOptions.set(unUsedOptions.size() - 1, lastOptions);
-            } else {
-                unUsedOptions.add(String.valueOf(minute) + ':' + possibleMove);
-                blizzardsState.put(minute, new ArrayList<>());
-                myState.put(minute, new Point(me.location));
-                for(Blizzard bl : blizzard)
-                    blizzardsState.get(minute).add(new Blizzard(bl));
+    //region Find the exact shortest path using the updateReachedList() method
+    private void findExactBestPath(int numSteps, Point start, Point end) {
+        if(numSteps > 0) {
+            resetBlizzardArray();
+            reached = new HashSet<>();
+            reached.add(new Point(start));
+            for(int i = 0; i < numSteps - 1; i++) {
+                updateBlizzardsPositions();
+                Set<Point> lastReached = reached;
+                reached = new HashSet<>();
+                reached.add(new Point(start));
+                for(Point p : lastReached)
+                    updateReachedList(p, end);
             }
+
+            if(reached.contains(new Point(end.x - 1, end.y))) {
+                steps.add(0, new Point(end.x - 1, end.y));
+            } else
+            if(reached.contains(new Point(end.x, end.y - 1))) {
+                steps.add(0, new Point(end.x, end.y - 1));
+            } else
+            if(reached.contains(new Point(end.x, end.y))) {
+                steps.add(0, new Point(end.x, end.y));
+            } else
+            if(reached.contains(new Point(end.x, end.y + 1))) {
+                steps.add(0, new Point(end.x, end.y + 1));
+            } else
+            if(reached.contains(new Point(end.x + 1, end.y))) {
+                steps.add(0, new Point(end.x + 1, end.y));
+            } else {
+                System.err.println("No previous point found!!!");
+            }
+
+            findExactBestPath(numSteps - 1, start, steps.get(0));
+        } else {
+            //Printing the exact path
+            for (Point p : steps)
+                System.out.printf("[%d, %d] -> ", p.x, p.y);
+            System.out.println("Win!");
         }
-
-        return nextMove;
     }
+    //endregion
 
-    //Next three methods compose the brute force solution (high complexity)
+    //region Brute force solution (high complexity)
     private int findBestPath_bruteForce() {
         blizzardsState = new HashMap<>();
         myState = new HashMap<>();
@@ -216,8 +250,26 @@ public class Day24_BlizzardBasin extends Puzzle {
         }
     }
 
-    //TODO: find the exact shortest path using the updateReachedList() method
-    private int findBestPath_iterativeNextToTheLast() {
-        return 0;
+    private char updatePossibleMove(char nextMove, char possibleMove) {
+        if(nextMove == ' ')
+            nextMove = possibleMove;
+        else {
+            String lastOptions = "";
+            if(unUsedOptions.size() > 0)
+                lastOptions = unUsedOptions.get(unUsedOptions.size() - 1);
+            if(lastOptions.startsWith(String.valueOf(minute))) {
+                lastOptions += possibleMove;
+                unUsedOptions.set(unUsedOptions.size() - 1, lastOptions);
+            } else {
+                unUsedOptions.add(String.valueOf(minute) + ':' + possibleMove);
+                blizzardsState.put(minute, new ArrayList<>());
+                myState.put(minute, new Point(me.location));
+                for(Blizzard bl : blizzard)
+                    blizzardsState.get(minute).add(new Blizzard(bl));
+            }
+        }
+
+        return nextMove;
     }
+    //endregion
 }
